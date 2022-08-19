ab4f0dc1543446938fb2f63f60fc523b12fce5a4
Adryan Ynfante <109553913+adryanYnfante@users.noreply.github.com>
2022-08-19 11:29:25 -0500

    ejercicos 2 declartivo

--- src/main/java/declarative1.java
+++ src/main/java/declarative1.java
@@ -1,3 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class declarative1 {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList( 11,22,33,44,55,66,77,88,99,100);
+        System.out.println(
+                numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .mapToInt(e->e*2).sum());
+    }
 }
